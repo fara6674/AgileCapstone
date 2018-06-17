b9ba9ba941b022c30082f59b64e64e7d570a2fa1
fara6674 <farrukhbek@gmail.com>
2018-06-16 21:45:15 -0500

    first commit

---
+++ src/app/signIn/signIn.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+ 
+@Component({
+    selector: 'base',
+    templateUrl: 'signIn.component.html'
+})
+export class SignInComponent {
+}
